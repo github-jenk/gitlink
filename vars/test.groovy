7baf9037b63b758c0b435263805f621f433c420e
anujgpt <40016981+anujgpt@users.noreply.github.com>
2018-07-16 11:18:20 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
 //#!/usr/bin/env groovy
 
-def call (String name = 'human') {
+def call (String name = 'test') {
   bat "curl -uadmin:APD7CbxbGBkHHLin -O \"http://localhost:8081/artifactory/local-my/${name}\""
 }
