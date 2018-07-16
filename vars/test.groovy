a0131f061ad1bfef57908d3c41c4cc59a9969e4e
anujgpt <40016981+anujgpt@users.noreply.github.com>
2018-07-16 10:49:56 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
 //#!/usr/bin/env groovy
 
 def call {
-bat "curl -uadmin:APD7CbxbGBkHHLin -O \"http://localhost:8081/artifactory/local-my/Vulnerabilities_10.6.15.20.json\""
+  bat "curl -uadmin:APD7CbxbGBkHHLin -O \"http://localhost:8081/artifactory/local-my/${name}\""
 }
